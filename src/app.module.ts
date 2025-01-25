689926f5224852cd5f1314d05c1deca1bbf8e6d9
diaa <mahmouddiaa13@gmail.com>
2025-01-25 21:23:14 +0200

    [logger] register RequestIdMiddleware to all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from './logger/logger.module';
+import { RequestIdMiddleware } from './request-id.middleware';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestIdMiddleware).forRoutes('*');
+  }
+}
