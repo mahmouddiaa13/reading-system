3337897ea4062587e7233af09cfbe360d562c523
diaa <mahmouddiaa13@gmail.com>
2025-01-26 09:29:37 +0200

    [config] module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
 import { RequestIdMiddleware } from './request-id.middleware';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [LoggerModule, ConfigModule.forRoot({})],
   controllers: [AppController],
   providers: [AppService],
 })
